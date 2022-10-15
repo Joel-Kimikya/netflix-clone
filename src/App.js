4ddc2c25a6820e142912554e4d7379ecddd37445
joelcena <joelcena.47@gmail.com>
2022-10-15 15:24:16 -0700

    we just done working on our displaying of Netflix Originals up to Documentaries Row

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Row from "./Row";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hey Clever Programmer</h1>
       <Row
-        title="NETFLIX ORIGINALS"
+        title='NETFLIX ORIGINALS'
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
-      <Row
-        title="Trending Now"
-        fetchUrl={requests.fetchTrending}
-      />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
